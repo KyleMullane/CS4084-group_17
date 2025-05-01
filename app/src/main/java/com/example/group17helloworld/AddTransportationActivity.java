a2379c00600f70247b2858d87bb4520f63f3e846
jasonmcgettrick <24462977@studentmail.ul.ie>
2025-05-01 16:42:17 +0100

    Added date and time functionality to transportation, and added lots of error handling to the add transportation class.

--- app/src/main/java/com/example/group17helloworld/AddTransportationActivity.java
+++ app/src/main/java/com/example/group17helloworld/AddTransportationActivity.java
@@ -2,6 +2,7 @@ package com.example.group17helloworld;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,7 +15,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddTransportationActivity extends AppCompatActivity {
     private static DBHandler database;
@@ -38,37 +44,135 @@ public class AddTransportationActivity extends AppCompatActivity {
 
     public void addTransportation(View view)
     {
+        boolean exceptionTriggered = false;
+        boolean dateExceptionTriggered = false;
+        boolean arrivalDateExceptionTriggered = false;
         try
         {
             // String departureLocation, String destination, String date, String departureTime, String arrivalTime, String type, double price, int tripID
-            EditText destinationText = findViewById(R.id.departureInput);
-            String destination = destinationText.getText().toString();
-
-            EditText departureText = findViewById(R.id.destinationInput);
+            EditText departureText = findViewById(R.id.departureInput);
             String departure = departureText.getText().toString();
+
+            EditText destinationText = findViewById(R.id.destinationInput);
+            String destination = destinationText.getText().toString();
 
             EditText dateText = findViewById(R.id.dateInput);
             String date = dateText.getText().toString();
+            TextView dateErrorMessage = findViewById(R.id.dateErrorMessage);
+            Date parsedDate;
+            try {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                sdf.setLenient(false);
+                parsedDate = sdf.parse(date);
+                dateErrorMessage.setText("");
+            }
+            catch (Exception e)
+            {
+                exceptionTriggered = true;
+                dateExceptionTriggered = true;
+                dateErrorMessage.setText("Error with departure date format. Make sure to use the format YYYY-MM-DD, with no spaces (even after). Sample date: 2025-05-02");
+            }
 
             EditText departureTimeText = findViewById(R.id.departureTimeInput);
             String departureTime = departureTimeText.getText().toString();
+            TextView departureTimeMessage = findViewById(R.id.departTimeMessage);
+            Date parsedDepartureTime = new Date();
+            try {
+                departureTimeMessage.setText("");
+                SimpleDateFormat timeSdf = new SimpleDateFormat("yyyy-MM-dd-HH:mm");
+                timeSdf.setLenient(false);
+                String dateTime = date + "-" + departureTime;
+                parsedDepartureTime = timeSdf.parse(dateTime);
+            }
+            catch (Exception e)
+            {
+                exceptionTriggered = true;
+                if (!dateExceptionTriggered)
+                {
+                    departureTimeMessage.setText("Error with departure time format. Make sure to use HH:mm with the 24 hour clock, and no spaces. Sample time: 15:31");
+                }
+            }
+
+            EditText arrivalDateText = findViewById(R.id.arrivalDateInput);
+            String arrivalDate = arrivalDateText.getText().toString();
+            TextView arrivalDateMessage = findViewById(R.id.arrivalDateMessage);
+            Date parsedArrivalDate;
+            try {
+                SimpleDateFormat arrivalSdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                arrivalSdf.setLenient(false);
+                parsedArrivalDate = arrivalSdf.parse(arrivalDate);
+                arrivalDateMessage.setText("");
+            }
+            catch (Exception e)
+            {
+                exceptionTriggered = true;
+                arrivalDateExceptionTriggered = true;
+                arrivalDateMessage.setText("Error with arrival date format. Make sure to use the format YYYY-MM-DD, with no spaces (even after). Sample date: 2025-05-02");
+            }
 
             EditText arrivalTimeText = findViewById(R.id.arrivalTimeInput);
             String arrivalTime = arrivalTimeText.getText().toString();
+            TextView arrivalTimeMessage = findViewById(R.id.arrivalTimeMessage);
+            Date parsedArrivalTime = new Date();
+            try {
+                arrivalTimeMessage.setText("");
+                SimpleDateFormat arrivalTimeSdf = new SimpleDateFormat("yyyy-MM-dd-HH:mm");
+                arrivalTimeSdf.setLenient(false);
+                String arrivalDateTime = arrivalDate + "-" + arrivalTime;
+                parsedArrivalTime = arrivalTimeSdf.parse(arrivalDateTime);
+            }
+            catch (Exception e)
+            {
+                exceptionTriggered = true;
+                if (!arrivalDateExceptionTriggered)
+                {
+                    arrivalTimeMessage.setText("Error with arrival time format. Make sure to use HH:mm with the 24 hour clock, and no spaces. Sample time: 15:31");
+                }
+            }
+            TextView wrongDates = findViewById(R.id.wrongDatesMessage);
+            wrongDates.setText("");
+            if (!exceptionTriggered)
+            {
+                if (parsedArrivalTime.before(parsedDepartureTime))
+                {
+                    exceptionTriggered = true;
+                    wrongDates.setText("Error: arrival date/time is before departure date/time");
+                }
+            }
+
 
             EditText typeText = findViewById(R.id.typeInput);
             String type = typeText.getText().toString();
 
             EditText priceText = findViewById(R.id.priceInput);
             String priceString = priceText.getText().toString();
-            double price = Double.parseDouble(priceString);
+            TextView costMessage = findViewById(R.id.wrongCostMessage);
+            double price = 0;
+            try {
+                costMessage.setText("");
+                price = Double.parseDouble(priceString);
+                if (price < 0)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                exceptionTriggered = true;
+                costMessage.setText("Invalid cost. Make sure to enter a numerical value that is positive");
+            }
 
-            database.addTransportation(new Transportation(departure,destination,date,departureTime,arrivalTime,type,price,trip.getTripID()));
-            Log.d("AddTransportationActivity", "Adding transportation to database was successful");
-            ArrayList<Transportation> transportationList = database.getTransportations();
-            Log.d("AddTransportationActivity","First item in transportation list is "+transportationList.get(0));
-            Intent homePageIntent = new Intent(this, HomePageActivity.class);
-            startActivity(homePageIntent);
+
+            if (!exceptionTriggered)
+            {
+                database.addTransportation(new Transportation(departure,destination,date,departureTime,arrivalTime,type,price,trip.getTripID()));
+                Log.d("AddTransportationActivity", "Adding transportation to database was successful");
+                ArrayList<Transportation> transportationList = database.getTransportations();
+                Log.d("AddTransportationActivity","First item in transportation list is "+transportationList.get(0));
+                Intent homePageIntent = new Intent(this, HomePageActivity.class);
+                startActivity(homePageIntent);
+            }
+
         }
         catch (Exception e)
         {
