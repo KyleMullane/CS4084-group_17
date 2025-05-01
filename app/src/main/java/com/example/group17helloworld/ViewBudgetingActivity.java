766f2106e6033c5f670c49dab7b828a171638275
jasonmcgettrick <24462977@studentmail.ul.ie>
2025-05-01 14:22:44 +0100

    Fleshed out the view budgeting activity. Should be ready to test

--- app/src/main/java/com/example/group17helloworld/ViewBudgetingActivity.java
+++ app/src/main/java/com/example/group17helloworld/ViewBudgetingActivity.java
@@ -3,6 +3,7 @@ package com.example.group17helloworld;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -61,19 +62,71 @@ public class ViewBudgetingActivity extends AppCompatActivity {
 
        double totalCost = transportationCost + accommodationCost + activitiesCost;
 
+        TextView titleText = findViewById(R.id.budgetingPageTitle);
+        titleText.setText("View Your Budgeting Details Below!");
+        TextView tableTitle = findViewById(R.id.tableTitle);
+        tableTitle.setText("Budgeting for your trip to "+trip.getDestination()+": ");
         TextView intendedBudgetText = findViewById(R.id.intendedBudget);
-        intendedBudgetText.setText("Your intended budget for this trip is: €"+trip.getBudget());
+        intendedBudgetText.setText("Intended Budget: €"+trip.getBudget());
         TextView actualCostText = findViewById(R.id.actualCost);
-        actualCostText.setText("Your logged spendings on this trip are: €"+totalCost);
+        actualCostText.setText("Logged Spendings: €"+totalCost);
 
         TextView transportationText = findViewById(R.id.transportationCost);
         TextView accommodationText = findViewById(R.id.accommodationCost);
         TextView activitiesText = findViewById(R.id.activitiesCost);
 
-        transportationText.setText("You have spent €"+transportationCost+" on transportation");
-        accommodationText.setText("You have spent €"+accommodationCost+" on accommodation");
-        activitiesText.setText("You have spent €"+activitiesCost+" on activities");
+        transportationText.setText("Transportation Cost: €"+transportationCost);
+        accommodationText.setText("Accommodation Cost: €"+accommodationCost);
+        activitiesText.setText("Activities cost: €"+activitiesCost);
+        TextView budgetFeedback = findViewById(R.id.budgetFeedback);
+        if (totalCost > trip.getBudget())
+        {
 
+            double maxCost = Math.max(transportationCost, Math.max(accommodationCost, activitiesCost));
+            if (transportationCost == maxCost && accommodationCost != maxCost && activitiesCost != maxCost)
+            {
+                budgetFeedback.setText("You are currently over budget by €"+(totalCost-trip.getBudget())+". Consider spending less on transportation");
+            }
+            else if (transportationCost != maxCost && accommodationCost == maxCost && activitiesCost != maxCost)
+            {
+                budgetFeedback.setText("You are currently over budget by €"+(totalCost-trip.getBudget())+". Consider spending less on accommodation");
+            }
+            else if (transportationCost != maxCost && accommodationCost != maxCost && activitiesCost == maxCost)
+            {
+                budgetFeedback.setText("You are currently over budget by €"+(totalCost-trip.getBudget())+". Consider spending less on activities");
+            }
+            else if (transportationCost == maxCost && accommodationCost == maxCost && activitiesCost != maxCost)
+            {
+                budgetFeedback.setText("You are currently over budget by €"+(totalCost-trip.getBudget())+". Consider spending less on transportation and accommodation");
+            }
+            else if (transportationCost == maxCost && accommodationCost != maxCost && activitiesCost == maxCost)
+            {
+                budgetFeedback.setText("You are currently over budget by €"+(totalCost-trip.getBudget())+". Consider spending less on transportation and activities");
+            }
+            else if (transportationCost != maxCost && accommodationCost == maxCost && activitiesCost == maxCost)
+            {
+                budgetFeedback.setText("You are currently over budget by €"+(totalCost-trip.getBudget())+". Consider spending less on accommodation and activities");
+            }
+            else
+            {
+                budgetFeedback.setText("You are currently over budget by €"+(totalCost-trip.getBudget())+". Reconsider your spendings!");
+            }
+        }
+        else if (totalCost < trip.getBudget())
+        {
+            budgetFeedback.setText("Nice job! You are under budget by €"+(trip.getBudget()-totalCost));
+        }
+        else
+        {
+            budgetFeedback.setText("Nice job! You have spent the exact amount of your planned budget. Try not to spend any more!");
+        }
+
+    }
+
+    public void sendToHomePage(View view)
+    {
+        Intent homePageIntent = new Intent(this, HomePageActivity.class);
+        startActivity(homePageIntent);
     }
 
 
